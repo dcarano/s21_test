53116e371f6a43c52c8736b438794faa8d98e127
dcarano <83577473+dcarano@users.noreply.github.com>
2021-05-17 13:28:19 -0700

    Update s21_test.py

--- s21_test.py
+++ s21_test.py
@@ -19,4 +19,5 @@ def remove(student_data):
     for key, value in student_data.items(): 
         if value not in result.values(): 
            result[key] = value
-    print ("result", str(result)) 
+           print (result[key])
+    
